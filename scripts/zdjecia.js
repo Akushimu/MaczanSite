b4157bd93c9cd90092d534190d84d4ec02061db8
axim4 <dynia.pl@gmail.com>
2020-09-13 12:50:59 +0200

    update

--- scripts/zdjecia.js
+++ scripts/zdjecia.js
@@ -4,5 +4,10 @@ numer_slajdu++;
 if(numer_slajdu==6)
 	numer_slajdu=1;
 document.getElementById("slider").innerHTML="<img src=\"images/slajd" + numer_slajdu + ".jpg\"/>";
+$("#slider").fadeIn(500);
 setTimeout("slajdy()",5000);
+setTimeout("schowaj()",4500);
+}
+function schowaj() {
+	$("#slider").fadeOut(500);
 }
