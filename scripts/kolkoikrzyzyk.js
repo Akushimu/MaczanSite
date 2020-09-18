b7eb205d3e8dbb8f0ddc49d5fb0ba6ddbf4c1e29
axim4 <dynia.pl@gmail.com>
2020-09-18 19:15:11 +0200

    update

--- scripts/kolkoikrzyzyk.js
+++ scripts/kolkoikrzyzyk.js
@@ -1,4 +1,13 @@
 function tictactoe() 
 {
-document.getElementById("tictac").innerHTML = `Do zrobienia`;
+	var pola="";
+	
+	for(i=0;i<=8;i++)
+	{
+		var elementk = "pk" + i;
+		pola = pola + `<div class ="polekik" onclick="" id="`+elementk+`"></div>`;
+		if((i+1)%3==0)pola = pola + '<div style="clear:both;"</div>'
+	}
+	
+	document.getElementById("tictac").innerHTML = pola;
 }
