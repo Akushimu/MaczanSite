57c6ca7305658ddf9c54023e43f06ead3030e791
axim4 <dynia.pl@gmail.com>
2020-09-18 22:09:58 +0200

    update

--- scripts/kolkoikrzyzyk.js
+++ scripts/kolkoikrzyzyk.js
@@ -1,12 +1,17 @@
+function wpisz(gdzie,co)
+{
+	document.getElementById(gdzie).innerHTML=co;
+}
+
 function tictactoe() 
 {
 	var pola="";
 	
 	for(i=0;i<=8;i++)
 	{
-		var elementk = "pk" + i;
-		pola = pola + `<div class ="polekik" onclick="" id="`+elementk+`"></div>`;
-		if((i+1)%3==0)pola = pola + '<div style="clear:both;"</div>'
+		var element = "p" + i;
+		pola = pola + '<div class ="polekik" onclick="wpisz(`p0`,`x`)" id="'+element+'"></div>';
+		if((i+1)%3==0)pola = pola + '<br>';
 	}
 	
 	document.getElementById("tictac").innerHTML = pola;
