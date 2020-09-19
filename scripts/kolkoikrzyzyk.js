3aaa12bad9b14176b418fdc10d5b603706840904
axim4 <dynia.pl@gmail.com>
2020-09-19 15:06:33 +0200

    update

--- scripts/kolkoikrzyzyk.js
+++ scripts/kolkoikrzyzyk.js
@@ -1,6 +1,73 @@
-function wpisz(gdzie,co)
+var plansza = ['','','',
+               '','','',
+			   '','',''];
+var count=0;
+var znak = "X";
+var stan = true;
+
+function stop()
 {
-	document.getElementById(gdzie).innerHTML=co;
+	stan = false;
+}
+
+function wygrana()
+{
+	if(plansza[0]=="X" && plansza[1]=="X" && plansza[2]=="X"
+	|| plansza[3]=="X" && plansza[4]=="X" && plansza[5]=="X"
+	|| plansza[6]=="X" && plansza[7]=="X" && plansza[8]=="X"
+	|| plansza[0]=="X" && plansza[4]=="X" && plansza[8]=="X"
+	|| plansza[2]=="X" && plansza[4]=="X" && plansza[6]=="X"
+	|| plansza[0]=="X" && plansza[3]=="X" && plansza[6]=="X"
+	|| plansza[1]=="X" && plansza[4]=="X" && plansza[7]=="X"
+	|| plansza[2]=="X" && plansza[5]=="X" && plansza[8]=="X") 
+	{
+		stop();
+		document.getElementById("wynikkk").innerHTML = "Zwyciężył krzyżyk";
+	}
+	else if(plansza[0]=="O" && plansza[1]=="O" && plansza[2]=="O"
+		 || plansza[3]=="O" && plansza[4]=="O" && plansza[5]=="O"
+		 || plansza[6]=="O" && plansza[7]=="O" && plansza[8]=="O"
+		 || plansza[0]=="O" && plansza[4]=="O" && plansza[8]=="O"
+		 || plansza[2]=="O" && plansza[4]=="O" && plansza[6]=="O"
+		 || plansza[0]=="O" && plansza[3]=="O" && plansza[6]=="O"
+		 || plansza[1]=="O" && plansza[4]=="O" && plansza[7]=="O"
+		 || plansza[2]=="O" && plansza[5]=="O" && plansza[8]=="O")
+		 {
+			 stop();
+		document.getElementById("wynikkk").innerHTML = "Wygrało kółko";
+	}
+	else if(count==8){
+		stop();
+		document.getElementById("wynikkk").innerHTML = "Remis";
+	}
+		 
+}
+function nextTura() 
+{
+	if(znak=="X")
+	{
+		znak="O";
+		count++;
+	}
+	else 
+	{
+		znak="X";
+		count++;
+	}
+}
+function wpisz(gdzie)
+{
+	if(stan == true)
+	{
+	var pp = gdzie.id.ustawZnak(0,"");
+	gdzie.innerHTML=znak;
+	plansza[pp]=znak;
+	if(count>=4)
+		wygrana();
+	nextTura();
+	gdzie.setAttribute("onclick",";");
+	gdzie.style.cursor = "default";
+	}
 }
 
 function tictactoe() 
@@ -10,7 +77,7 @@ function tictactoe()
 	for(i=0;i<=8;i++)
 	{
 		var element = "p" + i;
-		pola = pola + '<div class ="polekik" onclick="wpisz(`p0`,`x`)" id="'+element+'"></div>';
+		pola = pola + '<div class ="polekik" onclick="wpisz('+element+')" id="'+element+'"></div>';
 		if((i+1)%3==0)pola = pola + '<br/	>';
 	}
 	
