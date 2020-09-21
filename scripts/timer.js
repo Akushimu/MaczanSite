ea61418aa802d74443d914f54ed3dfa0f122245d
axim4 <dynia.pl@gmail.com>
2020-09-21 16:30:00 +0200

    update

--- scripts/timer.js
+++ scripts/timer.js
@@ -20,3 +20,26 @@ function odliczanie()
 		 
 		 setTimeout("odliczanie()",1000);
 	}
+
+function odliczanie2()
+	{
+		var dzisiaj = new Date();
+		
+		var dzien = dzisiaj.getDate();
+		var miesiac = dzisiaj.getMonth()+1;
+		var rok = dzisiaj.getFullYear();
+		
+		var godzina = dzisiaj.getHours();
+		if (godzina<10) godzina = "0"+godzina;
+		
+		var minuta = dzisiaj.getMinutes();
+		if (minuta<10) minuta = "0"+minuta;
+		
+		var sekunda = dzisiaj.getSeconds();
+		if (sekunda<10) sekunda = "0"+sekunda;
+		
+		document.getElementById("zegar").innerHTML = 
+		godzina+":"+minuta+":"+sekunda;
+		 
+		 setTimeout("odliczanie2()",1000);
+	}
