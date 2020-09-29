27cc90051dac0d3a68a31aee5bb66727dc618795
axim4 <dynia.pl@gmail.com>
2020-09-29 14:54:10 +0200

    update

--- scripts/sprawdzanie_liczb.js
+++ scripts/sprawdzanie_liczb.js
@@ -1,30 +1,30 @@
 function sprawdz_liczbe()
 {
   var liczba = document.getElementById("pole").value;
-  if(liczba>0) document.getElementById("wynik").innerHTML="Podana liczba jest dodatnia";
-  else if(liczba<0) document.getElementById("wynik").innerHTML="Podana liczba jest ujemna";
-  else if(liczba==0) document.getElementById("wynik").innerHTML="Podana liczba to zero";
-  else if(liczba=="JN") {
-	  document.getElementById("wynik").innerHTML=`<img src="images/rip.png" title="Jedna z ofiar śmiertelnych"><p>Jedna z ofiar śmiertelnych</p><img src="images/znicz.gif" title="Znicz">`;	  
-
-}
-   else if(liczba=="Chin Cheng Hanji") {
-	   document.write(`<video width="1920" height="1080" autoplay controls loop><source src="images/tom.mp4" type="video/mp4"></video>`);
+  if(liczba>0) $('#wynik').html("Podana liczba jest dodatnia");
+  else if(liczba<0) $('#wynik').html("Podana liczba jest ujemna");
+  else if(liczba==0) $('#wynik').html("Podana liczba to zero");
+  else if(liczba=="JN") 
+  {	  
+	  $('#wynik').html('<img src="images/rip.png" title="Jedna z ofiar śmiertelnych"><p>Jedna z ofiar śmiertelnych</p><img src="images/znicz.gif" title="Znicz">');
+  }
+   else if(liczba=="Ching Cheng Hanji") {
+	   document.write('<video width="1920" height="1080" autoplay controls loop><source src="images/tom.mp4" type="video/mp4"></video>');
   }
   else if(liczba=="JD") {
-	  document.getElementById("wynik").innerHTML="Oj tak tak byczq +1";	  
+	  $('#wynik').html("Oj tak tak byczq +1");	  
 
 }
 
   else if(liczba=="Dziady leczą") {
-	  document.getElementById("wynik").innerHTML=`<img src="images/dziady.jpg" alt="">`;	  
+	  $('#wynik').html('<img src="images/dziady.jpg" alt="">');	  
 
 }
 else if(liczba=="Koty") {
-	  document.getElementById("wynik").innerHTML=`<video width="640" height="368" autoplay controls loop><source src="images/koty.mp4" type="video/mp4"></video>`;	  
+	  $('#wynik').html('<video width="640" height="368" autoplay controls loop><source src="images/koty.mp4" type="video/mp4"></video>');	  
 
 }
 
-  else document.getElementById("wynik").innerHTML="Błąd";
+  else $('#wynik').html("Błąd");
 
 }
