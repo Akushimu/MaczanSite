8b1f1bc155858a006b2117448017a4a260f383b8
axim4 <dynia.pl@gmail.com>
2020-10-12 11:59:46 +0200

    Update checkingnumbers.js

--- scripts/checkingnumbers.js
+++ scripts/checkingnumbers.js
@@ -2,15 +2,17 @@ function checkInput()
 {
   var field = $('#pole');
   var result = $('#wynik');
-  var number = field.val();
+  var input = field.val();
 
-  if(number>0) result.html("Podana number jest dodatnia");
-  else if(number<0) result.html("Podana number jest ujemna");
-  else if(number==0) result.html("Podana number to zero");
-  else if(number=="JN") result.html('<img src="images/rip.png" title="Jedna z ofiar śmiertelnych"><p>Jedna z ofiar śmiertelnych</p><img src="images/znicz.gif" title="Znicz">');
-  else if(number=="Ching Cheng Hanji") document.write('<video width="1920" height="1080" autoplay loop><source src="images/tom.mp4" type="video/mp4"></video>');
-  else if(number=="JD") result.html("Oj tak tak byczq +1");	  
-  else if(number=="Dziady leczą") result.html('<img src="images/dziady.jpg" width="300">');	  
-  else if(number=="Koty") result.html('<video width="640" height="368" autoplay controls loop><source src="images/koty.mp4" type="video/mp4"></video>');
+  if(input>0 && input!=2137) result.html("Podana liczba jest dodatnia");
+  else if(input<0 ) result.html("Podana liczba jest ujemna");
+  else if(input==0) result.html("Podana liczba to zero");
+  else if(input=="JN") result.html('<img src="images/rip.png" title="Jedna z ofiar śmiertelnych"><p>Jedna z ofiar śmiertelnych</p><img src="images/znicz.gif" title="Znicz">');
+  else if(input=="Ching Cheng Hanji") document.write('<video width="1920" height="1080" autoplay loop><source src="images/tom.mp4" type="video/mp4"></video>');
+  else if(input=="JD") result.html("Oj tak tak byczq +1");	  
+  else if(input=="Dziady leczą") result.html('<img src="images/dziady.jpg" width="300">');	  
+  else if(input=="Koty") result.html('<video width="640" height="368" autoplay controls loop><source src="images/koty.mp4" type="video/mp4"></video>');
+  else if(input==2137) result.html('<iframe width="560" height="315" src="https://www.youtube.com/embed/Ec_kweeQM2A" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'); 
   else result.html("Błąd");
 }
+
