8fcca1e564fa1e900b134b6fcf08f79f79a94752
axim4 <dynia.pl@gmail.com>
2020-10-12 11:35:38 +0200

    Update checkingnumbers.js

--- scripts/checkingnumbers.js
+++ scripts/checkingnumbers.js
@@ -1,30 +1,16 @@
 function checkInput()
 {
-  var liczba = document.getElementById("pole").value;
-  if(liczba>0) $('#wynik').html("Podana liczba jest dodatnia");
-  else if(liczba<0) $('#wynik').html("Podana liczba jest ujemna");
-  else if(liczba==0) $('#wynik').html("Podana liczba to zero");
-  else if(liczba=="JN") 
-  {	  
-	  $('#wynik').html('<img src="images/rip.png" title="Jedna z ofiar śmiertelnych"><p>Jedna z ofiar śmiertelnych</p><img src="images/znicz.gif" title="Znicz">');
-  }
-   else if(liczba=="Ching Cheng Hanji") {
-	   document.write('<video width="1920" height="1080" autoplay controls loop><source src="images/tom.mp4" type="video/mp4"></video>');
-  }
-  else if(liczba=="JD") {
-	  $('#wynik').html("Oj tak tak byczq +1");	  
+  var field = $('#pole');
+  var result = $('#wynik');
+  var number = field.val();
 
-}
-
-  else if(liczba=="Dziady leczą") {
-	  $('#wynik').html('<img src="images/dziady.jpg" alt="">');	  
-
-}
-else if(liczba=="Koty") {
-	  $('#wynik').html('<video width="640" height="368" autoplay controls loop><source src="images/koty.mp4" type="video/mp4"></video>');	  
-
-}
-
-  else $('#wynik').html("Błąd");
-
+  if(number>0) result.html("Podana number jest dodatnia");
+  else if(number<0) result.html("Podana number jest ujemna");
+  else if(number==0) result.html("Podana number to zero");
+  else if(number=="JN") result.html('<img src="images/rip.png" title="Jedna z ofiar śmiertelnych"><p>Jedna z ofiar śmiertelnych</p><img src="images/znicz.gif" title="Znicz">');
+  else if(number=="Ching Cheng Hanji") document.write('<video width="1920" height="1080" autoplay loop><source src="images/tom.mp4" type="video/mp4"></video>');
+  else if(number=="JD") result.html("Oj tak tak byczq +1");	  
+  else if(number=="Dziady leczą") result.html('<img src="images/dziady.jpg" width="300">');	  
+  else if(number=="Koty") result.html('<video width="640" height="368" autoplay controls loop><source src="images/koty.mp4" type="video/mp4"></video>');
+  else result.html("Błąd");
 }
