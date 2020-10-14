ddc0e4d36428748af161d7061bb8960f57aa5833
axim4 <dynia.pl@gmail.com>
2020-10-14 13:53:57 +0200

    update

--- scripts/tictactoe.js
+++ scripts/tictactoe.js
@@ -1,80 +1,57 @@
-let board = ['','','',
-             '','','',
-			 '','',''];
-let count=0;
+let board = [];
+for(let i=0;i<=8;i++) board[i]=i;	 
+let count = 0;
 let mark = "X";
 
-function ticTacToe() 
-{
-	let fields="";
+function ticTacToe() {
+let fields="";
 	
-	for(i=0;i<=8;i++)
-	{
+	for(i=0;i<=8;i++) {
 		let elementId = "p" + i;
 		fields += '<div class = "polekik" onclick= "typeSmth('+i+')" id= "'+elementId+'"></div>';
 		if((i+1)%3==0)fields += '<br>';
 	}
-	
 	$('#tictac').html(fields);
 }
 
 
-function stop()
-{
+function stop() {
 	for(let i=0;i<=8;i++) {
 		$('#p'+i).attr("onclick",";");
 		$('#p'+i).css("cursor","default");
 	}
 }
 
-function checkIfWon()
-{
-	if(board[0]=="X" && board[1]=="X" && board[2]=="X"
-	|| board[3]=="X" && board[4]=="X" && board[5]=="X"
-	|| board[6]=="X" && board[7]=="X" && board[8]=="X"
-	|| board[0]=="X" && board[4]=="X" && board[8]=="X"
-	|| board[2]=="X" && board[4]=="X" && board[6]=="X"
-	|| board[0]=="X" && board[3]=="X" && board[6]=="X"
-	|| board[1]=="X" && board[4]=="X" && board[7]=="X"
-	|| board[2]=="X" && board[5]=="X" && board[8]=="X") 
-	{
-		$("#wynikkk").html("Zwyciężył krzyżyk");
+function checkIfWon() {
+	let winner;
+	if(mark == 'O') winner = 'krzyżyk';
+	else if(mark == 'X') winner = 'kółko';
+
+	if (board[0] == board[1] && board[1] == board[2] ||
+		board[3] == board[4] && board[4] == board[5] ||
+		board[6] == board[7] && board[7] == board[8] ||
+		board[0] == board[4] && board[4] == board[8] ||
+		board[2] == board[4] && board[4] == board[6] ||
+		board[0] == board[3] && board[3] == board[6] ||
+		board[1] == board[4] && board[4] == board[7] ||
+		board[2] == board[5] && board[5] == board[8]) {
+		$("#tttResult").html("Zwyciężył " + winner);
 		stop();
-	}
-	else if(board[0]=="O" && board[1]=="O" && board[2]=="O"
-		 || board[3]=="O" && board[4]=="O" && board[5]=="O"
-		 || board[6]=="O" && board[7]=="O" && board[8]=="O"
-		 || board[0]=="O" && board[4]=="O" && board[8]=="O"
-		 || board[2]=="O" && board[4]=="O" && board[6]=="O"
-		 || board[0]=="O" && board[3]=="O" && board[6]=="O"
-		 || board[1]=="O" && board[4]=="O" && board[7]=="O"
-		 || board[2]=="O" && board[5]=="O" && board[8]=="O")
-		{
-			for(let i=0;i<=8;i++) {
-				$('#p'+i).attr("onclick",";");
-				$('#p'+i).css("cursor","default");
-	
-			}
-			 $("#wynikkk").html("Wygrało kółko");
-			 stop();
 		}
-	else if(count==9){
-		$("#wynikkk").html("Remis");
+	else if(count==9) {
+		$("#tttResult").html("Remis");
 		stop();
 	}
-		 
 }
-function nextRound() 
-{
-	if(mark=="X")
-	{
-		$("#wynikkk").html("Tura kółka");
+
+function nextRound() {
+	if(mark=="X") {
+		$("#tttResult").html("Tura kółka");
 		mark="O";
 		count++;
 	}
-	else 
-	{
-		$("#wynikkk").html("Tura krzyżyka");
+	else {
+		$("#tttResult").html("Tura krzyżyka");
 		mark="X";
 		count++;
 	}
@@ -83,7 +60,7 @@ function nextRound()
 function typeSmth(smth) {
 	$('#p'+smth).html(mark);
 	board[smth]=mark;
-	nextRound();
+	if(count < 9) nextRound();
 	if(count >= 5) checkIfWon();
 	$('#p'+smth).attr("onclick",";");
 	$('#p'+smth).css("cursor","default");
