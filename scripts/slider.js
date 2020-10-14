3532698101f0eab707df6650a7589e2b8e3af1d0
axim4 <dynia.pl@gmail.com>
2020-10-14 14:04:46 +0200

    update

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,14 +1,12 @@
-var slideNumber=Math.floor(Math.random()*5)+1;
-
-function slider()
-{
-slideNumber++;
-if(slideNumber==6) slideNumber=1;
-$('#slider').html('<img src="images/slajd' + slideNumber + '.jpg" id="zslid">');
-$("#slider").fadeIn(500);
-setTimeout(slider,5000);
-setTimeout(hide,4500);
-}
-function hide() {
-	$("#slider").fadeOut(500);
+function slider() {
+	let slideNumber=Math.floor(Math.random()*5)+1;	
+	slideNumber++;
+	if(slideNumber==6) slideNumber=1;
+	$('#slider').html('<img src="images/slajd' + slideNumber + '.jpg" id="zslid">');
+	$("#slider").fadeIn(500);
+	setTimeout(slider,5000);
+	setTimeout(hide,4500);
+	}
+	function hide() {
+		$("#slider").fadeOut(500);
 }
